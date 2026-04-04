8af9d0944d9c08027a87d137d1532c60b036dfbe
SubhamLinux <linuxsubham@gmail.com>
2026-04-04 19:20:09 +0530

    Update func1.js

--- functions/func1.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const number = [1, 2, 3, 4, 5];
-  const [...dupNum] = number;
-  console.log(number);
-  console.log(dupNum);
-  console.log(...number);
-})();
-
-console.log("Task completed!");
